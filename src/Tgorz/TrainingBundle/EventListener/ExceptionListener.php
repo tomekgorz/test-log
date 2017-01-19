993b48a3f0990aaddffcd7985897a4607e07dcb3
Tomek gorz <gorztomasz@gmail.com>
2017-01-19 09:54:36 +0100

    translation

---
+++ src/Tgorz/TrainingBundle/EventListener/ExceptionListener.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tgorz\TrainingBundle\EventListener;
+
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
+class ExceptionListener {
+
+    public function onKernelException(GetResponseForExceptionEvent $event){
+        
+        $exception = $event->getException();
+        $message = sprintf('My Error says: %s with code: %s',
+                $exception->getMessage(),
+                $exception->getCode());
+        
+        $response = new Response();
+        
+        $response->setContent($message);
+        
+        if($exception instanceof HttpExceptionInterface){
+            $response->setStatusCode($exception->getStatusCode());
+            $response->headers->replace($exception->getHeaders());
+        }else{
+            $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+        
+        $event->setResponse($response);
+    }
+}
+
